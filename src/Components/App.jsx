fb666c777806cb59338de64db60eaa7b2db42408
rodrigo laime <rodrigolaime141@gmail.com>
2023-03-29 17:54:22 -0300

    not found add .

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path={'/'} element={<Login />} />
         <Route path={'/signup'} element={<Signup />} />
+        <Route path={'*'} element={<p>Not Fount</p>} />
       </Routes>
     </HashRouter>
   );
